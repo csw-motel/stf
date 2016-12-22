c3f647e506c71245cb446c03eb9f10c265917ae4
anaraquelazevedo <ana.azevedo04@gmail.com>
2016-12-22 22:20:09 +0000

    memory chart with zoom (first approach) bugs:    - zoom outbounds chart confusing code(to improve)

--- res/app/control-panes/performance/cpu/cpu-controller.js
+++ res/app/control-panes/performance/cpu/cpu-controller.js
@@ -2,91 +2,119 @@ module.exports = function CpuCtrl($scope, PerformanceService) {
 
   var d3 = require('d3')
 
+
+  var margin = {
+      top: 20,
+      right: 20,
+      bottom: 50,
+      left: 50
+    },
+    width = 400 - margin.left -
+    margin.right,
+    height = 250 - margin.top -
+    margin.bottom
+
+  var x = d3.time.scale()
+    .range([0, width])
+
+  var y = d3.scale.linear()
+    .range([height, 0])
+
+  var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom")
+
+  var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+
+  var line = d3.svg.line()
+    .x(function(d) {
+      return x(d.date)
+    })
+    .y(function(d) {
+      return y(d.value)
+    })
+    .interpolate("basis")
+
+  var svg = d3.select('#cpu svg')
+
+
+
+  svg = d3.select("#cpu").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top +
+      ")")
+
+
+  y.domain([0, 100])
+
+  var x_axis = svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis)
+
+  svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+
+
+  var color = d3.scale.ordinal().range(["#33cc33",
+    "#0099ff", "#ff9900", "#FF00FF", "#0D7AFF", "#6B238E",
+    "#FF6600", "#33ff33"
+  ])
+
+  //Create Y axis label
+  svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("%");
+
+  var legend = svg.selectAll('.g')
+    //  .data(cpus)
+    //  .enter()
+    //  .append('g')
+    //  .attr('class', 'legend');
+
+  legend.append('rect')
+    .attr('y', height + 30)
+    .attr('x', function(d, i) {
+      return i * 45;
+    })
+    .attr('width', 10)
+    .attr('height', 10)
+    .style('fill', function(d) {
+      return color(d.name);
+    });
+
+  legend.append('text')
+    .attr('y', height + 40)
+    .attr('x', function(d, i) {
+      return (i * 45) + 15;
+    })
+    .text(function(d) {
+      return d.name;
+    });
+
+
   var update = function() {
     var performanceData = PerformanceService.getPerformanceData
-
-    var margin = {
-        top: 20,
-        right: 20,
-        bottom: 50,
-        left: 50
-      },
-      width = parseInt(d3.select("#cpu").style("width")) - margin.left -
-      margin.right,
-      height = parseInt(d3.select("#cpu").style("height")) - margin.top -
-      margin.bottom
-
-    var x = d3.time.scale()
-      .range([0, width])
-
-    var y = d3.scale.linear()
-      .range([height, 0])
-
-    var xAxis = d3.svg.axis()
-      .scale(x)
-      .orient("bottom")
-
-    var yAxis = d3.svg.axis()
-      .scale(y)
-      .orient("left")
-
-    var line = d3.svg.line()
-      .x(function(d) {
-        return x(d.date)
-      })
-      .y(function(d) {
-        return y(d.value)
-      })
-      .interpolate("basis")
-
-    var svg = d3.select('#cpu svg')
-
-    d3.selectAll('#cpu svg').remove()
-
-    svg = d3.select("#cpu").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top +
-        ")")
-
 
     x.domain(d3.extent(performanceData, function(d) {
       return new Date(d.date * 1000);
     }));
 
 
-    y.domain([0, 100])
-
-    svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-
-    svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-
-
-    var color = d3.scale.ordinal().range(["#33cc33",
-      "#0099ff", "#ff9900", "#FF00FF", "#0D7AFF", "#6B238E",
-      "#FF6600", "#33ff33"
-    ])
-
-    //Create Y axis label
-    svg.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left)
-      .attr("x", 0 - (height / 2))
-      .attr("dy", "1em")
-      .style("text-anchor", "middle")
-      .text("%");
-
-
+    x_axis.call(xAxis)
     color.domain(d3.keys(performanceData[0]).filter(function(key) {
       return key !== "date";
     }));
@@ -103,38 +131,18 @@ module.exports = function CpuCtrl($scope, PerformanceService) {
       };
     });
 
-    var legend = svg.selectAll('.g')
-      .data(cpus)
-      .enter()
-      .append('g')
-      .attr('class', 'legend');
-
-    legend.append('rect')
-      .attr('y', height + 30)
-      .attr('x', function(d, i) {
-        return i * 45;
-      })
-      .attr('width', 10)
-      .attr('height', 10)
-      .style('fill', function(d) {
-        return color(d.name);
-      });
-
-    legend.append('text')
-      .attr('y', height + 40)
-      .attr('x', function(d, i) {
-        return (i * 45) + 15;
-      })
-      .text(function(d) {
-        return d.name;
-      });
 
 
     var cpu = svg.selectAll(".cpu")
       .data(cpus)
       .enter().append("g")
       .attr("class", "cpu")
-
+      .call(d3.behavior.zoom().scaleExtent([0.15, 12]).on("zoom", function() {
+        x_axis.call(xAxis);
+        cpu.attr("transform", "translate(" + d3.event.translate +
+          ")" +
+          " scale(" + d3.event.scale + ")")
+      }))
     cpu.append("path")
       .attr("class", "line")
       .attr("d", function(d) {
--- res/app/control-panes/performance/memory/memory-controller.js
+++ res/app/control-panes/performance/memory/memory-controller.js
@@ -2,125 +2,151 @@ module.exports = function MemoryCtrl($scope, PerformanceService) {
 
   var d3 = require('d3')
 
-  var update = function() {
-    var memoryData = PerformanceService.getMemoryData
-    var margin = {
-        top: 20,
-        right: 20,
-        bottom: 50,
-        left: 50
-      },
-      width = parseInt(d3.select("#memory").style("width")) - margin.left -
-      margin.right,
-      height = parseInt(d3.select("#memory").style("height")) - margin.top -
-      margin.bottom
 
-    var x = d3.time.scale()
-      .range([0, width])
+  var margin = {
+      top: 20,
+      right: 20,
+      bottom: 50,
+      left: 50
+    },
+    width = 500 - margin.left -
+    margin.right,
+    height = 250 - margin.top -
+    margin.bottom
 
-    var y = d3.scale.linear()
-      .range([height, 0])
+  var x = d3.time.scale()
+    .range([0, width])
 
-    var xAxis = d3.svg.axis()
+  var y = d3.scale.linear()
+    .range([height, 0])
+
+  var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom")
+
+  var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+
+  // function for the x grid lines
+  function make_x_axis() {
+    return d3.svg.axis()
       .scale(x)
       .orient("bottom")
+      .ticks(5)
+  }
 
-    var yAxis = d3.svg.axis()
+  // function for the y grid lines
+  function make_y_axis() {
+    return d3.svg.axis()
       .scale(y)
       .orient("left")
+      .ticks(5)
+  }
+  var area = d3.svg.area()
+    .x(function(d) {
+      return x(d.date);
+    })
+    .y0(height)
+    .y1(function(d) {
+      return y(d.value);
+    });
 
-    // function for the x grid lines
-    function make_x_axis() {
-      return d3.svg.axis()
-        .scale(x)
-        .orient("bottom")
-        .ticks(5)
-    }
+  var line = d3.svg.line()
+    .x(function(d) {
+      return x(d.date)
+    })
+    .y(function(d) {
+      return y(d.value)
+    })
+    .interpolate("basis")
 
-    // function for the y grid lines
-    function make_y_axis() {
-      return d3.svg.axis()
-        .scale(y)
-        .orient("left")
-        .ticks(5)
-    }
-    var area = d3.svg.area()
-      .x(function(d) {
-        return x(d.date);
-      })
-      .y0(height)
-      .y1(function(d) {
-        return y(d.value);
-      });
+  var svg = d3.select('#memory svg')
 
-    var line = d3.svg.line()
-      .x(function(d) {
-        return x(d.date)
-      })
-      .y(function(d) {
-        return y(d.value)
-      })
-      .interpolate("basis")
+  svg = d3.select("#memory").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top +
+      ")")
 
-    var svg = d3.select('#memory svg')
+  // Draw the x Grid lines
+  svg.append("g")
+    .attr("class", "grid")
+    .attr("transform", "translate(0," + height + ")")
+    .call(make_x_axis()
+      .tickSize(-height, 0, 0)
+      .tickFormat("")
+    )
 
-    d3.selectAll('#memory svg').remove()
+  // Draw the y Grid lines
+  svg.append("g")
+    .attr("class", "grid")
+    .call(make_y_axis()
+      .tickSize(-width, 0, 0)
+      .tickFormat("")
+    )
 
-    svg = d3.select("#memory").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top +
-        ")")
+  y.domain([0, PerformanceService.getMemTotal])
 
-    // Draw the x Grid lines
-    svg.append("g")
-      .attr("class", "grid")
-      .attr("transform", "translate(0," + height + ")")
-      .call(make_x_axis()
-        .tickSize(-height, 0, 0)
-        .tickFormat("")
-      )
+  var y_axis = svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    //Create Y axis label
+  svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("MB");
 
-    // Draw the y Grid lines
-    svg.append("g")
-      .attr("class", "grid")
-      .call(make_y_axis()
-        .tickSize(-width, 0, 0)
-        .tickFormat("")
-      )
+  var color = d3.scale.ordinal().range(["#b0c4de"])
+
+  var legend = svg.selectAll('.g')
+    //.data(memorys)
+    //.enter()
+    //.append('g')
+    //  .attr('class', 'legend');
+  legend.append('rect')
+    .attr('y', height + 30)
+    .attr('x', function(d, i) {
+      return i * 45;
+    })
+    .attr('width', 10)
+    .attr('height', 10)
+    .style('fill', function(d) {
+      return color(d.name);
+    });
+
+  legend.append('text')
+    .attr('y', height + 40)
+    .attr('x', function(d, i) {
+      return (i * 45) + 15;
+    })
+    .text(function(d) {
+      return d.name;
+    });
+
+  var x_axis = svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis)
+
+
+
+  var update = function() {
+    var memoryData = PerformanceService.getMemoryData
 
     x.domain(d3.extent(memoryData, function(d) {
       return new Date(d.date * 1000);
     }));
 
-
-    y.domain([0, PerformanceService.getMemTotal])
-
-    svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-
-    svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      //Create Y axis label
-    svg.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left)
-      .attr("x", 0 - (height / 2))
-      .attr("dy", "1em")
-      .style("text-anchor", "middle")
-      .text("MB");
-
-    var color = d3.scale.ordinal().range(["#b0c4de"])
-
-
+    x_axis.call(xAxis)
     color.domain(d3.keys(memoryData[0]).filter(function(key) {
       return key !== "date";
     }));
@@ -137,38 +163,13 @@ module.exports = function MemoryCtrl($scope, PerformanceService) {
       };
     });
 
-    var legend = svg.selectAll('.g')
-      .data(memorys)
-      .enter()
-      .append('g')
-      .attr('class', 'legend');
-
-    legend.append('rect')
-      .attr('y', height + 30)
-      .attr('x', function(d, i) {
-        return i * 45;
-      })
-      .attr('width', 10)
-      .attr('height', 10)
-      .style('fill', function(d) {
-        return color(d.name);
-      });
-
-    legend.append('text')
-      .attr('y', height + 40)
-      .attr('x', function(d, i) {
-        return (i * 45) + 15;
-      })
-      .text(function(d) {
-        return d.name;
-      });
-
     var memory = svg.selectAll(".memory")
       .data(memorys)
       .enter().append("g")
       .attr("class", "memory")
 
-    memory.append("path")
+
+    var path = memory.append("path")
       .attr("class", "area")
       .attr("d", function(d) {
         return area(d.values)
@@ -176,7 +177,16 @@ module.exports = function MemoryCtrl($scope, PerformanceService) {
       .style("stroke", function(d) {
         return color(d.name)
       })
-
+      .call(d3.behavior.zoom().scaleExtent([0.15, 12])
+        .x(x).on("zoom", function() {
+          x_axis.call(xAxis);
+          memory.attr("transform", "translate(" + d3.event.translate +
+            ")" +
+            " scale(" + d3.event.scale + ")")
+        }))
   }
+
   setInterval(update, 1000)
+
+
 }
